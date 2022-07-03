215fad3d5c833fa0962c59cf31c8ef9b13cc7186
DevopsGuyXD <77780574+DevopsGuyXD@users.noreply.github.com>
2022-07-04 00:38:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,9 @@ const github = require('@actions/github');
 
 try {
 
-  exec(`aws configure set aws_access_key_id ${access_key_id}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      console.log(`stdout: ${stdout}`);
- 	});
-
-
-	exec(`aws configure set aws_secret_access_key ${access_key_secret}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      console.log(`stdout: ${stdout}`);
-    });
-
-
-	exec(`aws configure set region ${region}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-      console.log(`stdout: ${stdout}`);
-    });
-
+  app.get("/test", (_req, res) =>  {
+   res.status(200).send("Hello world")
+  });
 
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
