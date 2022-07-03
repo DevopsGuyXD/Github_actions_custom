745d3ed5383c664a333a0bca6b5cc7688e3e8749
DevopsGuyXD <bharathdundi@gmail.com>
2022-07-04 02:05:51 +0530

    push

---
+++ index.js
@@ -0,0 +1,47 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('exec');
+
+try {
+
+  const access_key_id     = core.getInput('access_key_id');
+  const access_key_secret = core.getInput('access_key_secret');
+  const region            = core.getInput('region');
+
+  exec(`aws configure set aws_access_key_id ${access_key_id}`, (error, stdout) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+  });
+
+
+  exec(`aws configure set aws_secret_access_key ${access_key_secret}`, (error, stdout) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+  });
+
+
+  exec(`aws configure set region ${region}`, (error, stdout) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+  });
+
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+
+
+} catch (error) {
+
+  core.setFailed(error.message);
+
+}
