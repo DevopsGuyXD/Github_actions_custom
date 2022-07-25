b8be23df66a35cc94df6285a81a5a8de8f415fd4
DevopsGuyXD <bharathdundi@gmail.com>
2022-07-26 01:28:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,6 +31,22 @@ try {
       }
   });
 
+
+  exec(`cd ..`, (error, stdout) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+  });
+
+
+  exec(`ls -a`, (error, stdout) => {
+      if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+      }
+  });
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
