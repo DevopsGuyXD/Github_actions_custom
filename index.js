0596eb4803560c1ba2400884a0d3883e9dd939cd
DevopsGuyXD <77780574+DevopsGuyXD@users.noreply.github.com>
2022-07-26 02:10:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,36 +8,6 @@ try {
   const access_key_secret = core.getInput('access_key_secret');
   const region            = core.getInput('region');
 
-  exec(`aws configure set aws_access_key_id ${access_key_id}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-  });
-
-
-  exec(`aws configure set aws_secret_access_key ${access_key_secret}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-  });
-
-
-  exec(`aws configure set region ${region}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-  });
-
-
-  exec(`docker build -t testimage .`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-  });
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
