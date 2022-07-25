fd841d1a7371940884be1ed7955dc58af0283ab2
DevopsGuyXD <bharathdundi@gmail.com>
2022-07-26 02:37:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,26 +9,26 @@ try {
   const region            = core.getInput('region');
   const docker_image_name = core.getInput('docker_image_name');
 
-  exec(`aws configure set aws_access_key_id ${access_key_id}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-  });
+  // exec(`aws configure set aws_access_key_id ${access_key_id}`, (error, stdout) => {
+  //     if (error) {
+  //         console.log(`error: ${error.message}`);
+  //         return;
+  //     }
+  // });
 
-  exec(`aws configure set aws_secret_access_key ${access_key_secret}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-  });
+  // exec(`aws configure set aws_secret_access_key ${access_key_secret}`, (error, stdout) => {
+  //     if (error) {
+  //         console.log(`error: ${error.message}`);
+  //         return;
+  //     }
+  // });
 
-  exec(`aws configure set region ${region}`, (error, stdout) => {
-      if (error) {
-          console.log(`error: ${error.message}`);
-          return;
-      }
-  });
+  // exec(`aws configure set region ${region}`, (error, stdout) => {
+  //     if (error) {
+  //         console.log(`error: ${error.message}`);
+  //         return;
+  //     }
+  // });
 
   exec(`docker build -t ${docker_image_name} .`, (error, stdout) => {
       if (error) {
