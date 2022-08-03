3a8ff253dca612e159e53408bb8911f3c94f2afa
DevopsGuyXD <bharathdundi@gmail.com>
2022-08-03 23:32:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,15 +15,7 @@ try {
   const cluster_name         = core.getInput('cluster_name');
   const service_name         = core.getInput('service_name');
 
-  exec(`aws configure set aws_access_key_id ${access_key_id};
-        aws configure set aws_secret_access_key ${access_key_secret};
-        aws configure set region ${region};
-        ${ecr_login};
-        docker build -t ${docker_image_name}:latest . --build-arg env=${env};
-        docker build -t ${docker_image_name}:v.${tag.substring(0,8)} . --build-arg env=${env};
-        docker push ${docker_image_name}:latest;
-        docker push ${docker_image_name}:v.${tag.substring(0,8)};
-        aws ecs update-service --cluster ${cluster_name} --service ${service_name} --force-new-deployment`, (error, stdout) => {
+  exec(`aws configure set aws_access_key_id ${access_key_id} && aws configure set aws_secret_access_key ${access_key_secret} && aws configure set region ${region} && ${ecr_login} && docker build -t ${docker_image_name}:latest . --build-arg env=${env} && docker build -t ${docker_image_name}:v.${tag.substring(0,8)} . --build-arg env=${env} && docker push ${docker_image_name}:latest && docker push ${docker_image_name}:v.${tag.substring(0,8)} && aws ecs update-service --cluster ${cluster_name} --service ${service_name} --force-new-deployment`, (error, stdout) => {
       if (error) {
           console.log(`error: ${error.message}`);
           return;
