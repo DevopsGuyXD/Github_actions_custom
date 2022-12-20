15cdb0824b9b193613d6e888cd1f57dbea5d80b3
DevopsGuyXD <bharathdundi@gmail.com>
2022-12-21 02:14:12 +0530

    App service update

--- README.md
+++ README.md
@@ -1,3 +1,74 @@
-# Github_actions_custom
+# Deploy to AWS ECS
 
-<h4>- You can find the pipelines in the respective branches of this repository</h4>
+<br>
+<h2>Secrets:</h2>
+<h4>1. Create these secrets in your github projects repository</h4>
+
+    ACTION:
+  	- Add this value "git clone -b ecs https://github.com/DevopsGuyXD/Github_actions_custom.git"
+
+    ACCESS_KEY_ID:         
+	- AWS access key
+
+    ACCESS_KEY_SECRET:     
+	- AWS access key secret
+
+    REGION:                
+	- AWS region
+
+    ECR_LOGIN:             
+	- AWS ECR login command
+
+    DOCKER_IMAGE_NAME:     
+	- Name for the docker image
+
+    DOCKER_FILE_LOCATION:  
+	- Location of your dockerfile
+
+    ENV:
+	- Env for the docker image Example-Prod, Dev etc
+
+    CLUSTER_NAME:          
+	- Name the AWS Cluster
+
+    SERVICE_NAME:          
+	- Name of the service running in the custer
+
+
+<br>
+<h2>Pipeline:</h2>
+<h4>2. Create a folder named ".github" at the root level of your project, Create a file with the name "deploy_to_ecs.yml" and add the below contents to it while setting the branch value as well</h4>
+
+    on:
+      push:
+        branches: [ Enter the branch from which your code needs to get deployed ]
+
+    name: Deploy to Amazon ECS
+
+    jobs:
+      deploy:
+        name: Deploy
+        runs-on: ubuntu-18.04
+
+        steps:
+        - name: Checkout
+          uses: actions/checkout@v2
+
+        - name: Download action
+          run: |
+            ${{ secrets.ACTION }}
+
+        - name: Deploy to ECS
+          id: Deploy_to_ECS
+          uses: ./Github_actions_custom
+          with:
+            access_key_id: ${{ secrets.ACCESS_KEY_ID }}
+            access_key_secret: ${{ secrets.ACCESS_KEY_SECRET }}
+            region: ${{ secrets.REGION }}
+            ecr_login: ${{ secrets.ECR_LOGIN }}
+            docker_image_name: ${{ secrets.DOCKER_IMAGE_NAME }}
+            docker_file_location: ${{ secrets.DOCKER_FILE_LOCATION }}
+            env: ${{ secrets.ENV_INT }}
+            tag: ${{ github.sha }}
+            cluster_name: ${{ secrets.CLUSTER_NAME }}
+            service_name: ${{ secrets.SERVICE_NAME }}
---
+++ index.js
@@ -0,0 +1,42 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("child_process").exec;
+
+try {
+  const docker_server_url                       = core.getInput("docker_server_url");
+  const docker_server_username                  = core.getInput("docker_server_username");
+  const docker_server_password                  = core.getInput("docker_server_password");
+  const docker_image_name                       = core.getInput("docker_image_name");
+  const github_username                         = core.getInput("github_username");
+  const github_pat                              = core.getInput("github_pat");
+  const resource_group                          = core.getInput("resource_group")
+  const app_service_withfloats_api              = core.getInput("app_service_withfloats_api")
+  const app_service_withfloats_api_createfp     = core.getInput("app_service_withfloats_api_createfp")
+  const app_service_withfloats_api_getfpdetails = core.getInput("app_service_withfloats_api_getfpdetails")
+  const app_service_withfloats_api_getkeywords  = core.getInput("app_service_withfloats_api_getkeywords")
+  const app_service_withfloats_api_search       = core.getInput("app_service_withfloats_api_search")
+
+  exec(
+    `docker login ${docker_server_url} --username ${docker_server_username} --password ${docker_server_password} && \
+     docker build -t ${docker_server_url}/${docker_image_name}:latest . --build-arg USERNAME=${github_username} --build-arg PAT=${github_pat} && \
+     docker build -t ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} . --build-arg USERNAME=${github_username} --build-arg PAT=${github_pat} && \
+     docker push ${docker_server_url}/${docker_image_name}:latest && \
+     docker push ${docker_server_url}/${docker_image_name}:v.${tag.substring(0, 8)} && \
+     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api} --resource-group ${resource_group}
+     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_createfp} --resource-group ${resource_group}
+     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_getfpdetails} --resource-group ${resource_group}
+     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_getkeywords} --resource-group ${resource_group}
+     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_search} --resource-group ${resource_group}`,
+    (error, stdout) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      } else {
+        console.log(`Output: ${stdout}`);
+        return;
+      }
+    }
+  );
+} catch (error) {
+  core.setFailed(error.message);
+}
