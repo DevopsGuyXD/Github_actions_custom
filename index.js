2aba17682c7314d72202f8367a279ef97594701e
DevopsGuyXD <bharathdundi@gmail.com>
2022-12-21 17:35:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,16 +19,9 @@ try {
   const app_service_withfloats_api_search = core.getInput("app_service_withfloats_api_search");
 
   exec(
-    `docker login ${docker_server_url} --username ${docker_server_username} --password ${docker_server_password} && \
-     docker build -t ${docker_server_url}/${docker_image_name}:latest ${docker_file_path} --build-arg USERNAME=${github_username} --build-arg PAT=${github_pat} && \
-     docker build -t ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} ${docker_file_path} --build-arg USERNAME=${github_username} --build-arg PAT=${github_pat} && \
-     docker push ${docker_server_url}/${docker_image_name}:latest && \
-     docker push ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} && \
-     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api} --resource-group ${resource_group} && \
-     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_createfp} --resource-group ${resource_group} && \
-     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_getfpdetails} --resource-group ${resource_group} && \
-     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_getkeywords} --resource-group ${resource_group} && \
-     az webapp config container set --docker-custom-image-name ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} --name ${app_service_withfloats_api_search} --resource-group ${resource_group}`,
+    `docker login jiwwithfloatsapiecrstaging.azurecr.io --username jiwwithfloatsapiecrstaging --password FEYhiwr1YhySz5TA3Y2A9+C16KtJwupM && \
+     docker build -t jiwwithfloatsapiecrstaging.azurecr.io/withfloats-api:test ./src --build-arg USERNAME="BharathDundi1" --build-arg PAT="d6ff6206eebe54f18166609536a4cf8db5d183d8" && \
+     docker push jiwwithfloatsapiecrstaging.azurecr.io/withfloats-api:test`,
     (error, stdout) => {
       if (error) {
         console.log(`error: ${error.message}`);
