7172970d94740e2c685a8ea139e52c886a2e7d78
DevopsGuyXD <77780574+DevopsGuyXD@users.noreply.github.com>
2022-12-21 14:29:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ try {
   const app_service_withfloats_api_search = core.getInput("app_service_withfloats_api_search");
 
   exec(
-    `docker login ${docker_server_url} --username ${docker_server_username} --password ${docker_server_password} && \
+    `ls -a && \
+     docker login ${docker_server_url} --username ${docker_server_username} --password ${docker_server_password} && \
      docker build -t ${docker_server_url}/${docker_image_name}:latest ${docker_file_path} --build-arg USERNAME=${github_username} --build-arg PAT=${github_pat} && \
      docker build -t ${docker_server_url}/${docker_image_name}:v.${tag.substring(0,8)} ${docker_file_path} --build-arg USERNAME=${github_username} --build-arg PAT=${github_pat} && \
      docker push ${docker_server_url}/${docker_image_name}:latest && \
